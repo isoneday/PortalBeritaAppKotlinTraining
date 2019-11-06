4a5a8105f2e492a8f66c9034e4133dbfdea725f3
isoneday <iswandi.saputra11@gmail.com>
2019-11-06 13:15:36 +0700

    Initial commit

---
+++ app/src/main/java/com/imastudio/portalberitaapp/activity/SplashScreenActivity.kt
@@ -0,0 +1,27 @@
+package com.imastudio.portalberitaapp.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.imastudio.portalberitaapp.MainActivity
+import com.imastudio.portalberitaapp.R
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        lottie1.setAnimation("AndroidWave.json")
+        lottie1.loop(true)
+        lottie1.playAnimation()
+
+        //untuk melakukan penundaan ke suatu aksi /activity
+        val handler = Handler()
+        handler.postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 3000)
+
+    }
+}
